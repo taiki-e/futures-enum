ae98b02341c07d15665a0d22ef54dd9fe2c1f54f
Taiki Endo <te316e89@gmail.com>
2020-10-25 23:05:41 +0900

    Expand glob imports

--- tests/test.rs
+++ tests/test.rs
@@ -1,7 +1,7 @@
 #![warn(rust_2018_idioms, single_use_lifetimes)]
 #![allow(dead_code)]
 
-use futures_enum::*;
+use futures_enum::{AsyncBufRead, AsyncRead, AsyncSeek, AsyncWrite, Future, Sink, Stream};
 
 #[derive(Future, Stream, Sink, AsyncRead, AsyncWrite, AsyncSeek, AsyncBufRead)]
 enum Either<A, B> {
