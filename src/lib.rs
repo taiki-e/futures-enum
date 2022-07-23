1d8b63d3a93a16fa8108baa2da18693ed2ac7063
Taiki Endo <te316e89@gmail.com>
2022-07-24 03:09:31 +0900

    Apply clippy pedantic lints

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,7 @@
 ))]
 #![forbid(unsafe_code)]
 #![warn(rust_2018_idioms, single_use_lifetimes, unreachable_pub)]
-#![warn(clippy::default_trait_access, clippy::wildcard_imports)]
+#![warn(clippy::pedantic)]
 
 // older compilers require explicit `extern crate`.
 #[allow(unused_extern_crates)]
