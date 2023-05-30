f58941d5701607946687a74588b12412ba48869e
Taiki Endo <te316e89@gmail.com>
2023-05-31 04:12:36 +0900

    Add assertions to test

--- tests/test.rs
+++ tests/test.rs
@@ -1,10 +1,23 @@
 #![warn(rust_2018_idioms, single_use_lifetimes)]
 #![allow(dead_code)]
 
+use core::future::Future;
+
+use futures::{AsyncBufRead, AsyncRead, AsyncSeek, AsyncWrite, Sink, Stream};
 use futures_enum::{AsyncBufRead, AsyncRead, AsyncSeek, AsyncWrite, Future, Sink, Stream};
 
 #[derive(Future, Stream, Sink, AsyncRead, AsyncWrite, AsyncSeek, AsyncBufRead)]
 enum Either<A, B> {
     A(A),
     B(B),
+}
+
+fn _assert_impl<
+    T: Future + Stream + Sink<()> + AsyncRead + AsyncWrite + AsyncSeek + AsyncBufRead,
+>() {
+    fn __assert_impl<
+        T: Future + Stream + Sink<()> + AsyncRead + AsyncWrite + AsyncSeek + AsyncBufRead,
+    >() {
+    }
+    __assert_impl::<Either<T, T>>();
 }
